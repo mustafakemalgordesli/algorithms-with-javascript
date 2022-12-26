f0be0da8d32cc19d0adee99f76fa0a548b0c21fc
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2022-12-26 19:37:46 +0300

    sort algorithms added

---
+++ sort/bubbleSort.js
@@ -0,0 +1,20 @@
+const bubbleSort = (arr) => {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i + 1] < arr[i]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+};
+
+const arr = [-6, 20, 8, -2, 4];
+bubbleSort(arr);
+console.log(arr);
+
+// Big-O = O(n^2)
---
+++ sort/insertionSort.js
@@ -0,0 +1,17 @@
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length - 1; i++) {
+    const numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+};
+
+const arr = [-6, 20, 8, -2, 4];
+insertionSort(arr);
+console.log(arr);
+
+// Big-O = O(n^2)
